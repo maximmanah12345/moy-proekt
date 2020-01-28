a4aa072184d221e1402ca3fa309150bdfc9e2ea4
maximmanah12345 <55143520+maximmanah12345@users.noreply.github.com>
2020-01-28 17:00:08 +0300

    Add files via upload

---
+++ proekt maksima/proekt maksima/Form2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proekt_maksima
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3("Far Cry 3");
+            f.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3("Far Cry 4");
+            f.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3("Far Cry 5");
+            f.Show();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3("Far Cry Primal");
+            f.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Form3 f = new Form3("Far Cry New Dawn");
+            f.Show();
+        }
+    }
+}
---
+++ proekt maksima/proekt maksima/Form3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proekt_maksima
+{
+    public partial class Form3 : Form
+    {
+        public Form3(string game)
+        {
+            InitializeComponent();
+
+            if (game == "Far Cry 3")
+            {
+                Text = "Far Cry 3";
+                pictureBox1.Load("../../Resources/Far Cry 3.jpg");
+            }
+
+            if (game == "Far Cry 4")
+            {
+                Text = "Far Cry 4";
+                pictureBox1.Load("../../Resources/Far Cry 4.jpg");
+            }
+
+            if (game == "Far Cry 5")
+            {
+                Text = "Far Cry 5";
+                pictureBox1.Load("../../Resources/Far Cry 5.jpg");
+            }
+
+            if (game == "Far Cry Primal")
+            {
+                Text = "Far Cry Primal";
+                pictureBox1.Load("../../Resources/Far Cry Primal.jpg");
+            }
+
+            if (game == "Far Cry New Dawn")
+            {
+                Text = "Far Cry New Dawn";
+                pictureBox1.Load("../../Resources/Far Cry New Dawn.jpg");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
