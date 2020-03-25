ed6e0b18bae8db106a1d00341024aa74a247cb88
Максим <maks0073@outlook.com>
2020-03-25 16:54:45 +0400

    dfghh

--- proekt maksima/Category.Designer.cs
+++ proekt maksima/Category.Designer.cs
@@ -40,7 +40,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(59, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(233, 77);
             this.button1.TabIndex = 2;
@@ -57,7 +57,7 @@
             "Гонки"});
             this.checkedListBox1.Location = new System.Drawing.Point(468, 12);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(221, 89);
+            this.checkedListBox1.Size = new System.Drawing.Size(221, 88);
             this.checkedListBox1.TabIndex = 22;
             // 
             // gamePanel
@@ -81,7 +81,7 @@
             // 
             // Category
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(826, 488);
             this.Controls.Add(this.button2);
